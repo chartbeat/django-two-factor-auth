9c236ca48798087e8131f5e65f53eb6529019f65
Usama Hussain <usama@chartbeat.com>
2020-11-20 10:59:48 -0600

    [HULK-388] Make django_two_factor compatible with django3

--- two_factor/auth_backends.py
+++ two_factor/auth_backends.py
@@ -3,20 +3,23 @@ from django.utils.timezone import now
 from oath import accept_totp
 from django.conf import settings
 
-TF_FORWARD_DRIFT = getattr(settings,'TF_FORWARD_DRIFT', 1)
-TF_BACKWARD_DRIFT = getattr(settings,'TF_BACKWARD_DRIFT', 1)
+TF_FORWARD_DRIFT = getattr(settings, "TF_FORWARD_DRIFT", 1)
+TF_BACKWARD_DRIFT = getattr(settings, "TF_BACKWARD_DRIFT", 1)
 
 
 class TokenBackend(ModelBackend):
-    def authenticate(self, user, token):
-        accepted, drift = accept_totp(key=user.tf_token.seed, response=token,
-                                      forward_drift=TF_FORWARD_DRIFT,
-                                      backward_drift=TF_BACKWARD_DRIFT)
+    def authenticate(self, request, user, token):
+        accepted, drift = accept_totp(
+            key=user.tf_token.seed,
+            response=token,
+            forward_drift=TF_FORWARD_DRIFT,
+            backward_drift=TF_BACKWARD_DRIFT,
+        )
         return user if accepted else None
 
 
 class VerifiedComputerBackend(ModelBackend):
-    def authenticate(self, user, computer_id):
+    def authenticate(self, request, user, computer_id):
         verification = user.tf_verified_computers.get(pk=computer_id)
         if verification.verified_until < now():
             return None
